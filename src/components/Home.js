f63c3535418fb43377713786ffb744d9eef69ed0
beverlyperez <42483610+beverlyperez@users.noreply.github.com>
2020-10-08 23:11:19 -0700

    changed the daisy and lily names to show

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,45 @@
 import React from 'react';
-
+import Login from './Login';
 class Home extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            showLoginForm: false
+        };
+
+    }
+
+    login = () => {
+        this.setState({
+            showLoginForm: true
+        });
+
+    }
+
     render(){
+        if(this.state.showLoginForm){
+            return (
+                <div>
+                    <form>
+                        <button onClick={this.login}>Login</button>    
+                        hello from the home page
+                    
+                    </form>
+                    
+                    </div>
+    
+            );
+
+        }
         return (
-            <div>hello from the home page</div>
+            <div>
+                <form>
+                    <button onClick={this.login}>Login</button>    
+                    hello from the home page
+                
+                </form>
+                
+                </div>
 
         );
     }
