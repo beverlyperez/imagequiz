4fbd451c60eca86409bca4ded0391388931966fd
beverlyperez <42483610+beverlyperez@users.noreply.github.com>
2020-10-23 10:01:13 -0700

    tesitng past did not work

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -164,43 +164,43 @@ class HomePage extends React.Component {
                 </div>  
             );
         }
-        else if(this.state.results && this.state.quizVideoG){
+        if(this.state.results && this.state.quizVideoG){
             return(
                 <div className="Results">
-                    <h1>Your results are: </h1>
+                    <h1>Your results are: </h1><br></br>
                     <p>{this.state.amountCorrect}</p>
-                    <p>Out of 6 correct</p>
-                    <button className="returnHome" onClick={this.EmptyAll}>Go Home</button><br></br>
+                    <p>   Out of 6 correct</p>
+                    <button className="returnHome" onClick={this.EmptyAll}>Go Home</button><br></br><br></br>
                     <button className="TryAgain" onClick={this.restartVideoG}>Try Again</button><br></br>
                     
                 </div>
             );
         }
-        else if(this.state.results && this.state.quizCnCart){
+        if(this.state.results && this.state.quizCnCart){
             return(
                 <div className="Results">
-                    <h1>Your results are: </h1>
+                    <h1>Your results are: </h1><br></br>
                     <p>{this.state.amountCorrect}</p>
-                    <p>Out of 6 correct</p>
-                    <button className="returnHome" onClick={this.EmptyAll}>Go Home</button><br></br>
+                    <p>   Out of 6 correct</p>
+                    <button className="returnHome" onClick={this.EmptyAll}>Go Home</button><br></br><br></br>
                     <button className="TryAgain" onClick={this.restartCnCart}>Try Again</button><br></br>
                     
                 </div>
             );
         }
-        else if(this.state.results && this.state.quizHall){
+        if(this.state.results && this.state.quizHall){
             return(
                 <div className="Results">
-                    <h1>Your results are: </h1>
+                    <h1>Your results are: </h1><br></br>
                     <p>{this.state.amountCorrect}</p>
-                    <p>Out of 6 correct</p>
+                    <p>    Out of 6 correct</p>
                     <button className="returnHome" onClick={this.EmptyAll}>Go Home</button><br></br>
                     <button className="TryAgain" onClick={this.restartHall}>Try Again</button><br></br>
                     
                 </div>
             );
         }
-        else if(this.state.quizCnCart){
+        if(this.state.quizCnCart){
             return (
                 <div className="CNQ">
                     <h1>Which cartoon show from Cartoon Network, does the picture show?</h1>
@@ -272,7 +272,7 @@ class HomePage extends React.Component {
                 </div>
             );
         }
-        else if(this.state.quizHall){
+        if(this.state.quizHall){
             return (
                 <div className="HALL">
                     <h1>What is the name of this halloween item?</h1>
@@ -343,7 +343,7 @@ class HomePage extends React.Component {
                 </div>
             );
         }
-        else if(this.state.quizVideoG){
+        if(this.state.quizVideoG){
             return (
                 <div className="VG">
                     <h1>From which video game is the picture from?</h1>
